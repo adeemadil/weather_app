21664ab64c55de0d7888d430caf66836abc48ba6
Adeem Adil Khatri <adil.khatri@ug.bilkent.edu.tr>
2023-02-16 19:25:21 +0500

    Add: Map and search option

--- src/App.js
+++ src/App.js
@@ -1,59 +1,77 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
+import WeatherMap from "./Components/WeatherMap/WeatherMap";
 import WeatherWidget from "./Components/WeatherWidget/WeatherWidget";
 
 import "./App.css";
 
-const lat = 33.6844;
-const lon = 73.0479;
-
-const API_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
+//Islamabad Coordinates
 
 function App() {
   const [weatherData, setWeatherData] = useState([{}, {}, {}, {}, {}]);
+  const [lat, setLat] = useState(33.6844);
+  const [lon, setLon] = useState(73.0479);
+
+  const API_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
+
+  const locationChangeHandler = useCallback(
+    (lat, lon) => {
+      setLat(lat);
+      setLon(lon);
+      axios
+        .get(API_URL)
+        .then((res) => {
+          if (res.status === 200) {
+            console.log(res);
+            const dailyTemp = res.data.daily;
+
+            let weatherDataTemp = [];
+            let minDays = Math.min(5, dailyTemp.length);
+            for (let i = 0; i < minDays; i++) {
+              let daily = dailyTemp[i];
+
+              weatherDataTemp.push({
+                date: new Date(daily["dt"] * 1000),
+                max: Math.round(daily["temp"]["max"]),
+                min: Math.round(daily["temp"]["min"]),
+                description: daily["weather"][0]["description"],
+                windSpeed: daily["wind_speed"],
+                humidity: daily["humidity"],
+                pressure: daily["pressure"],
+                icon: daily["weather"][0]["icon"],
+              });
+            }
+
+            console.log(weatherDataTemp);
+
+            setWeatherData(weatherDataTemp);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+          alert("Some Error Occurred!");
+        });
+    },
+    [API_URL]
+  );
 
   useEffect(() => {
-    axios
-      .get(API_URL)
-      .then((res) => {
-        if (res.status === 200) {
-          console.log(res);
-          const dailyTemp = res.data.daily;
-
-          let weatherDataTemp = [];
-          let minDays = Math.min(5, dailyTemp.length);
-          for (let i = 0; i < minDays; i++) {
-            let daily = dailyTemp[i];
-
-            weatherDataTemp.push({
-              date: new Date(daily["dt"] * 1000),
-              max: Math.round(daily["temp"]["max"]),
-              min: Math.round(daily["temp"]["min"]),
-              description: daily["weather"][0]["description"],
-              windSpeed: daily["wind_speed"],
-              humidity: daily["humidity"],
-              pressure: daily["pressure"],
-              icon: daily["weather"][0]["icon"],
-            });
-          }
-
-          console.log(weatherDataTemp);
-
-          setWeatherData(weatherDataTemp);
-        }
-      })
-      .catch((e) => {
-        console.log(e);
-        alert("Some Error Occurred!");
-      });
-  }, []);
+    locationChangeHandler(lat, lon);
+  }, [lat, locationChangeHandler, lon]);
 
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <section>Map</section>
-      <section>Search</section>
+      <div className="map-container">
+        <section>
+          <WeatherMap
+            latt={lat}
+            lon={lon}
+            onLocationChange={locationChangeHandler}
+          />
+        </section>
+      </div>
       <div className="weather-cards-container">
         <div id="weatherContainer">
           {weatherData.map((data, i) => (
--- src/Components/WeatherMap/WeatherMap.jsx
+++ src/Components/WeatherMap/WeatherMap.jsx
@@ -1,2 +1,63 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "mapbox-gl/dist/mapbox-gl.css"; // import the CSS file
+// import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
 import "./styles/WeatherMap.css";
+
+const WeatherMap = ({ lon, latt, onLocationChange }) => {
+  const mapContainer = useRef(null);
+  const coordsRef = useRef({ lat: latt, lng: lon });
+  const [zoom, setZoom] = useState(13);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [coordsRef.current.lng, coordsRef.current.lat],
+      zoom: zoom,
+    });
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+    });
+    geocoder.on("result", (event) => {
+      coordsRef.current.lng = event.result.center[0];
+      coordsRef.current.lat = event.result.center[1];
+      console.log(event.result.center[0], event.result.center[1]);
+      setZoom(12);
+      console.log("lat", coordsRef.current.lat, "lng", coordsRef.current.lng);
+      onLocationChange(coordsRef.current.lat, coordsRef.current.lng);
+    });
+    map.addControl(geocoder);
+
+    map.on("move", () => {
+      console.log(
+        map.getCenter().lng.toFixed(4),
+        map.getCenter().lat.toFixed(4)
+      );
+      coordsRef.current.lng = map.getCenter().lng.toFixed(4);
+      coordsRef.current.lat = map.getCenter().lat.toFixed(4);
+      setZoom(map.getZoom().toFixed(2));
+      console.log("lat", coordsRef.current.lat, "lng", coordsRef.current.lat);
+      onLocationChange(coordsRef.current.lat, coordsRef.current.lng);
+    });
+
+    return () => map.remove();
+  }, [coordsRef, zoom, onLocationChange]);
+
+  return (
+    <div>
+      <div className="map-container" ref={mapContainer} />
+      <div className="sidebar">
+        Longitude: {coordsRef.current.lng} | Latitude: {coordsRef.current.lat} |
+        Zoom: {zoom}
+      </div>
+    </div>
+  );
+};
+
+export default WeatherMap;
